aaed1731d14fcf40fb61aed6debe00798b6438dc
utkr07 <sutkarsh0719@gmail.com>
2023-10-20 00:20:27 +0530

    Added helper function

---
+++ src/components/helpers/protectedRoutes.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import { Navigate } from "react-router-dom";
+
+export default function UserSignedIn({ user, children }) {
+  if (user) {
+    return <Navigate to="'/'" replace />;
+  }
+
+  return children;
+}
