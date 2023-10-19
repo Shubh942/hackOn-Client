94deb992c7eb9caaa5f90fc25d3a646d0bb0d8ed
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-10-19 23:36:40 +0530

    added styles

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
-export const API_KEY = process.env.REACT_APP_TMDB_APIKEY;
+export const API_KEY = "a6cb561e9fb0bf81d331032d3b9deb15";
 export const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 export const YOUTUBE_URL = "https://www.youtube.com/watch?v=";
